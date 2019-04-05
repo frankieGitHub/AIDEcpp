450b3ee534b16a12aa7c0e8c2ac750a5bd9ce51c
frankie <frankie_lrh@163.com>
2019-04-05 13:09:37 +0800

    add find value in a tree

--- MyNativeExecutable2/jni/hello.cpp
+++ MyNativeExecutable2/jni/hello.cpp
@@ -4,6 +4,32 @@
 
 using namespace std;
 
+///find a value in a tree start
+struct tNode {
+	int data;
+	
+	struct tNode* left;
+	struct tNode* right;
+};
+
+struct tNode* findValue(struct tNode* root, int data)
+{
+	if(root->data == data) return root;
+	
+	struct tNode* temp1 = findValue(root->left, data);
+	if(temp1 != NULL) return temp1;
+	
+	struct tNode* temp2 = findValue(root->right, data);
+	if(temp2 != NULL) return temp2;
+	
+	return NULL;
+}
+///find a value in a tree end
+
+
+
+
+
 int main()
 {
 	vector<int> myArrays;
